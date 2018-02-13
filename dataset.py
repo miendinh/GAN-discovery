c7d90b3f500a39db6191f1a7784896092ffe7c5b
miendinh <miendinh@luxcer.com>
2018-02-13 21:47:40 +0700

    add vietocr dataset

--- dataset.py
+++ dataset.py
@@ -1,32 +1,124 @@
-import matplotlib.pyplot as plt
-import cv2
-import random
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import numpy as np
+import random
+import sys
+from config import *
 
-images = []
-size = 210
 
-def rgba2rbg(image):
-    return cv2.cvtColor(image, cv2.COLOR_RGBA2RGB)
+class DataSet:
+    def __init__(self, test_prob=0.2, one_hot=True):
+        self.test_prob = test_prob
+        self.one_hot = one_hot
+        self.X_test = []
+        self.Y_test = []
+        self.curr_training_step = 0
+        self.curr_test_step = 0
+        self.line_offset = self.get_line_offset()
+        self.train_idx, self.test_idx = self.shuffle_data_set()
 
-def load_imgs():
-    global images
-    for i in range(1, size + 1):
-        image = plt.imread('dataset/flower_images/{0:04d}.png'.format(i))
-        image = rgba2rbg(image)
-        images.append(image)
-    random.shuffle(images)
-    return images
+    def to_one_hot(self, X):
+        one_hot = np.zeros((len(X), NO_LABEL))
+        for i in range(len(X)):
+            np.put(one_hot[i, :], X[i], 1)
 
-def next_batch(step, batch_size):
-    global images
-    if images == []:
-        print('loading...')
-        images = load_imgs()
-    return np.array(images[step:step+batch_size])
+        return one_hot
 
-# if __name__ == '__main__':
-#     images = next_batch(0, 10)
-#     plt.imshow(images[0], cmap='gray')
-#     plt.show()
-#     print(images.shape)
+    def shuffle_data_set(self):
+        idx = list(range(SUM_SAMPLES))
+
+        for i in range(5):
+            random.shuffle(idx)
+
+        test_size = int(self.test_prob * SUM_SAMPLES)
+
+        test_idx = idx[0:test_size]
+        train_idx = idx[test_size:SUM_SAMPLES]
+        print('Done shuffle dataset!');
+        return (train_idx, test_idx)
+
+    def next_batch(self, batch_size):
+        idx = self.train_idx[self.curr_training_step * batch_size:self.curr_training_step * batch_size + batch_size]
+        data = []
+
+        with open(DATASET_FILE, 'r') as ds:
+            for i in range(len(idx)):
+                ds.seek(self.line_offset[idx[i]])
+                line = ds.readline().strip()
+                data.append(line.split(','))
+
+        X_train_bs, Y_train_bs = self.split_image_label(data)
+
+        self.curr_training_step = self.curr_training_step + 1
+
+        if (self.curr_training_step * batch_size > len(self.train_idx)):
+            self.curr_training_step = 0
+            random.shuffle(self.train_idx)
+
+        return (X_train_bs, Y_train_bs)
+
+    def next_batch_test(self, batch_size):
+        idx = self.test_idx[self.curr_test_step * batch_size:self.curr_test_step * batch_size + batch_size]
+        data = []
+        debug_offset = None
+        debug_idx = None
+        with open(DATASET_FILE, 'r') as ds:
+            for i in range(len(idx)):
+                debug_idx = idx[i]
+                debug_offset = self.line_offset[idx[i]]
+                ds.seek(self.line_offset[idx[i]])
+                line = ds.readline().strip()
+                data.append(line.split(','))
+
+        X_test_bs, Y_test_bs = self.split_image_label(data)
+
+        self.curr_test_step = self.curr_test_step + 1
+
+        if (self.curr_test_step * batch_size > len(self.test_idx)):
+            self.curr_test_step = 0
+            random.shuffle(self.test_idx)
+
+        return (X_test_bs, Y_test_bs)
+
+    def split_image_label(self, data):
+        if (type(data) is list):
+            data = np.asarray(data, dtype=np.float32)
+
+        image = data[:, :-1]
+        label = data[:, -1]
+        if self.one_hot:
+            return (image, self.to_one_hot(label))
+        else:
+            return (image, label)
+
+    def get_test_set(self):
+
+        if len(self.X_test) > 0 and len(self.Y_test) > 0:
+            return (self.X_test, Y_test)
+
+        idx = self.test_idx
+        data = []
+        with open(DATASET_FILE, 'r') as ds:
+            for i in range(len(idx)):
+                ds.seek(self.line_offset[idx[i]])
+                line = ds.readline().replace('\n', '')
+                data.append(line.split(','))
+
+        self.X_test, self.Y_test = self.split_image_label(data)
+
+        return (self.X_test, self.Y_test)
+
+    def get_line_offset(self):
+        line_offset = []
+        offset = 0
+        with open(DATASET_FILE, 'r') as ds:
+            for line in ds:
+                line_offset.append(offset)
+                offset += len(line)
+
+        return line_offset
+
+
+        # if __name__ == "__main__":
+        #   prepare = DataSet()
--- model.py
+++ model.py
@@ -2,98 +2,119 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-from dataset import next_batch
+from dataset import DataSet
+import sys
 
-batch_size = 10;
-gen_size = 1;
-epochs = 3000;
-k = 21;
+batch_size = 100;
+sample_size = 100;
+epochs = 3001;
+steps = 1000;
 Z_dimension = 100;
+dataset = DataSet(test_prob=0, one_hot=False)
 
-def conv2d(X, filters, kernel_size = 5, strides = 2, padding = 'same', is_training = True):
+def conv2d(X, filters, kernel_size = 2, strides = 2, padding = 'same', is_training = True):
     X = tf.layers.conv2d(X, filters, kernel_size, strides = strides, padding = padding )
     X = tf.layers.batch_normalization(X, training = is_training)
     X = tf.nn.leaky_relu(X)
     return X
 
-def deconv(Z, filters, kernel_size = 5, strides = 2, padding = 'same', is_training = True):
+def deconv(Z, filters, kernel_size = 2, strides = 2, padding = 'same', is_training = True):
     Z = tf.layers.conv2d_transpose(Z, filters, kernel_size, strides = strides, padding= padding)
     Z = tf.layers.batch_normalization(Z, training = is_training)
     Z = tf.nn.relu(Z)
     return Z
 
-def Generator(Z, reuse = False, is_training = True):
+def Generator(Z, reuse = False, is_training = False):
     with tf.variable_scope("Generator") as scope:
         if reuse:
             scope.reuse_variables()
 
-        Z = tf.layers.dense(Z, units=4*4*1024)
+        Z = tf.layers.dense(Z, units=3*3*128)
         Z = tf.layers.batch_normalization(Z, training = is_training)
         Z = tf.nn.relu(Z)
-        Z = tf.reshape(Z, shape=[-1, 4, 4, 1024])
-        Z = deconv(Z, 1024)
-        Z = deconv(Z, 512)
-        Z = deconv(Z, 256)
-        Z = deconv(Z, 128)
-        Z = deconv(Z, 3)
+        Z = tf.reshape(Z, shape=[-1, 3, 3, 128])
+        Z = deconv(Z, 128, kernel_size = 3, padding = 'valid')
+        Z = deconv(Z, 64)
+        Z = deconv(Z, 1)
         Z = tf.nn.tanh(Z)
         return Z
 
-def Discriminator(X, reuse = False, is_training = True):
+def Discriminator(X, reuse = False, is_training = False):
     with tf.variable_scope("Discriminator") as scope:
         if reuse:
             scope.reuse_variables()
+
         X = conv2d(X, 32, is_training = is_training)
         X = conv2d(X, 64, is_training = is_training)
-        X = conv2d(X, 128, is_training = is_training)
-        X = conv2d(X, 256, is_training = is_training)
-        X = conv2d(X, 512, is_training = is_training)
-        X = conv2d(X, 1024, is_training = is_training)
-
-        X = tf.reshape(X, shape=[-1, 2*2*1024])
-        X = tf.layers.dense(X, 1024)
+        X = conv2d(X, 128, kernel_size = 3, padding = 'valid', is_training = is_training)
+        X = tf.reshape(X, shape=[-1, 3*3*128])
+        X = tf.layers.dense(X, 128)
         X = tf.layers.batch_normalization(X, training = is_training)
         X = tf.layers.dense(X, 1)
 
-        return X
+        return tf.nn.sigmoid(X), X
 
 Z = tf.placeholder(tf.float32, shape=(None, 100), name = 'Z')
-X = tf.placeholder(tf.float32, shape=(None, 128, 128, 3), name = 'X')
+X = tf.placeholder(tf.float32, shape=(None, 28, 28, 1), name = 'X')
 
 G = Generator(Z)
-Dx = Discriminator(X)
-Dg = Discriminator(G, reuse = True)
+Dx_s, Dx = Discriminator(X)
+Dg_s, Dg = Discriminator(G, reuse = True)
 
-G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Dg, labels=tf.ones_like(Dg)))
-Dx_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Dx, labels=tf.fill([batch_size, 1], 1.)))
-Dg_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Dg, labels=tf.zeros_like(Dg)))
-
+Dx_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Dx, labels=tf.ones_like(Dx_s)))
+Dg_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Dg, labels=tf.zeros_like(Dg_s)))
 D_loss = Dx_loss + Dg_loss
+G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Dg, labels=tf.ones_like(Dg_s)))
+
+tf.summary.scalar('Dx_loss', Dx_loss)
+tf.summary.scalar('Dg_loss', Dg_loss)
+tf.summary.scalar('D_loss',  D_loss)
+tf.summary.scalar('G_loss',  G_loss)
+
+summary = tf.summary.merge_all()
 
 G_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='Generator')
 D_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='Discriminator')
 
 with tf.variable_scope(tf.get_variable_scope(), reuse = False) as scope:
-    Dx_trainner = tf.train.AdamOptimizer(0.001).minimize(Dx, var_list = D_vars)
-    Dg_trainner = tf.train.AdamOptimizer(0.001).minimize(Dg, var_list = D_vars)
-    G_trainner = tf.train.AdamOptimizer(0.001).minimize(G_loss, var_list = G_vars)
+    Dx_trainner = tf.train.AdamOptimizer(0.0001).minimize(Dx_loss, var_list = D_vars)
+    Dg_trainner = tf.train.AdamOptimizer(0.0001).minimize(Dg_loss, var_list = D_vars)
+    G_trainner = tf.train.AdamOptimizer(0.0001).minimize(G_loss, var_list = G_vars)
 
-noises = np.random.uniform(-1., 1., size = [gen_size, Z_dimension])
+sample_noises = np.random.uniform(-1., 1., size = [sample_size, Z_dimension])
+sample_images, _ = dataset.next_batch(batch_size)
+sample_images = sample_images.reshape((batch_size, 28, 28, 1))
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
+    writer = tf.summary.FileWriter('./logs', sess.graph)
+
     for e in range(epochs):
-        Z_random_noises = np.random.uniform(-1., 1., [batch_size, Z_dimension])
-        for i in range(k):
-            images = next_batch(i, batch_size)
-            _, _, D_loss_ = sess.run([Dx_trainner, Dg_trainner, D_loss], {X : images, Z : Z_random_noises }) #is_training : True
+        for i in range(steps):
+            images, _ = dataset.next_batch(batch_size)
+            if len(images) < batch_size:
+                continue
+            images = images.reshape((batch_size, 28, 28, 1))
 
-        _, G_loss_ = sess.run([G_trainner, G_loss], feed_dict={Z : Z_random_noises}) #is_training : True
+            Z_random_noises = np.random.uniform(-1., 1., [batch_size, Z_dimension])
 
-        print("G_loss = ", G_loss_, "D_loss = ", D_loss_)
+            _, _, Dx_loss_, Dg_loss_, summary_ = sess.run([Dx_trainner, Dg_trainner, Dx_loss, Dg_loss, summary], {X : images, Z : Z_random_noises })
 
-        if e % 2 == 0:
-            Z_gen, Dl, Gl = sess.run([G, D_loss, G_loss], {X: images, Z: noises})
-            #plt.imshow(Z_gen[0,:,:,:].reshape(128, 128, 3))
-            #plt.show()
-            cv2.imwrite("generated/{0:05d}.jpg".format(e), Z_gen[0,:,:,:].reshape(128, 128, 3))
+            _, G_loss_ = sess.run([G_trainner, G_loss], feed_dict={Z : Z_random_noises})
+            _, G_loss_ = sess.run([G_trainner, G_loss], feed_dict={Z : Z_random_noises})
+
+            print("Epoch:%04d Step: %04d G_loss: %.8f Dx_loss: %.8f Dg_loss: %.8f" % (e, i, G_loss_, Dx_loss_, Dg_loss_))
+
+        writer.add_summary(summary_, e + 1)
+
+        if e % 1 == 0:
+            Z_gen = sess.run(G, feed_dict={Z: sample_noises})
+            Z_gen = np.array(Z_gen.reshape((sample_size, 28, 28)) + 1) * 127.5
+            big_image = Z_gen.reshape(10, 10, 28, 28).swapaxes(1,2).reshape(10*28, 10*28)
+            plt.imshow(big_image, cmap='gray')
+            plt.show()
+            cv2.imwrite("generated/{0:05d}.jpg".format(e), big_image)
+
+    saver = tf.train.Saver()
+    save_path = saver.save(sess, "./gan_discover.ckpt")
+    print('done!')
